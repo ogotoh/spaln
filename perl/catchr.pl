b36c51274c93b38ab53cde14c57097054d50394e
Osamu Gotoh <o.gotoh@aist.go.jp>
2019-03-29 13:32:49 +0900

    update readme.html

--- perl/catchr.pl
+++ perl/catchr.pl
@@ -1,8 +1,41 @@
 #!/usr/bin/perl
 
-# Catenate separate chromosomal sequences in a single multi-fasta file
-# Copyright(c) Osamu Gotoh <<o.gotoh@i.kyoto-u.ac.jp>>
-# <2007-07-23>
+# Catenate a single or separate chromosomal sequences in a single multi-fasta file
+# Copyright(c) Osamu Gotoh <<o.gotoh@i.aist.go.jp>>
+#
+# Input: FASTA, Genbank/DDBJ, or EMBL format sequence files
+# Output: to STDOUT in FASTA format, in which
+# from the top of sequence ID to the 'Keyword' (inclusive) will be replaced by 'Prefix'
+#
+# Example:
+# catch,pl -wAADE -hDrospseu AAED.gbk 
+# will converte 
+#
+# Input: AAED.gbk
+#--------------------------------------------------------------------------------
+# LOCUS       AADE02000001           28560 bp    DNA     linear   INV 26-JUN-2018
+# DEFINITION  Drosophila pseudoobscura pseudoobscura strain MV2-25 contig_1,
+#            whole genome shotgun sequence.
+# annotations
+# ORIGIN
+# sequence
+# //
+# LOCUS       AADE02000002           22723 bp    DNA     linear   INV 26-JUN-2018
+# ...
+#--------------------------------------------------------------------------------
+#
+# to
+#
+#--------------------------------------------------------------------------------
+# >Drospseu02000001
+# sequence
+# >Drospseu02000002
+# ...
+#--------------------------------------------------------------------------------
+#
+# <2019-03-28>
+
+use strict;
 
 my $nbr;
 my $oriid = 1;
@@ -13,9 +46,13 @@ my $omit_old_id;
 my $Un = "Un";
 my $field = -1;
 my $addchr;
+my $gbk;
+my $embl;
+my $anno;
 
 sub usage {
-    print STDERR "catchr -hPrefix [-n] [-o][-wWord] files > target\n";
+    print "Usage:\n";
+    print "\tcatchr.pl [-wKeyword] [-hPrefix] [-n] [-o] input_files > target\n";
     exit (1);
 }
 
@@ -23,6 +60,7 @@ while ($_ = $ARGV[0], /^-/) {
     shift;
     /^-c(\S*)/	&& ($addchr = $1);
     /^-f(\d*)/	&& ($field = $1);
+    /^-h$/	&& &usage;
     /^-h(\S+)/	&& ($prefix = $1);
     /^-i/	&& ($oriid = 0);
     /^-n/	&& ($addnbr = 1);
@@ -52,7 +90,7 @@ while (<>) {
 	    print "_$nbr" if ($addnbr);
 	    my $oldid = $1;
 	    if ($omit_old_id) {
-		(my $dummy, $oldid) = split(' ', $1, 2);
+		(undef, $oldid) = split(' ', $1, 2);
 	    }
 	    print " $oldid$3\n";
 	} elsif ($addnbr) {
@@ -63,6 +101,33 @@ while (<>) {
 	} else {
 	    print ">$prefix", substr($_,1);
 	}
+    } elsif (/^LOCUS\s+(\S+)/) {
+	my $id = $1;
+	if ($idword && $id =~ /^(.*)$idword\s*(\S+)(.*)/o) {
+	    $id = $2;
+	}
+	print ">$prefix$id\n";
+	$gbk = $anno = 1;
+    } elsif (/^ID\s+(\S+)/) {
+	my $id = $1;
+	if ($idword && $id =~ /^(.*)$idword\s*(\S+)(.*)/o) {
+	    $id = $2;
+	}
+	print ">$prefix$id\n";
+	$embl = $anno = 1;
+    } elsif (/^\//) {
+	$anno = 1;
+    } elsif ($anno) {
+	$anno = 0 if (/^ORIGIN/ || /^SQ/);
+    } elsif ($gbk) {
+	$_ = substr($_, 10);
+	tr / //d;
+	print;
+    } elsif ($embl) {
+	chomp;
+	$_ = substr($_, 5, 65);
+	tr / //d;
+	print $_, "\n";
     } else {
 	print;
     }
